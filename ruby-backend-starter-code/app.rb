31e787290c9d664638ad2fb249afdb94df7ed436
Dominic Wong <dominic.wong.617@gmail.com>
2015-12-10 00:10:04 +0800

    Fixed bugs in sinatra backend

--- ruby-backend-starter-code/app.rb
+++ ruby-backend-starter-code/app.rb
@@ -1,20 +1,55 @@
 require 'sinatra'
+require 'json'
 
-get '/'
-  File.read('index.html')
+# NOTE: the filename where the data
+# is located (a constant)
+DB_FILENAME = 'data.json'
+
+helpers do
+  # NOTE: if the file data.json does not exist,
+  # return an empty string
+  def read_file
+    begin
+      File.read(DB_FILENAME)
+    rescue Errno::ENOENT
+      ''
+    end
+  end
+
+  # NOTE: if the data is not valid JSON,
+  # return an empty array
+  def read_data
+    begin
+      JSON.parse(read_file)
+    rescue JSON::ParserError
+      []
+    end
+  end
+
+  def write_data(data)
+    File.write(DB_FILENAME, JSON.pretty_generate(data))
+  end
 end
 
-get 'favorites' do
-  response.header['Content-Type'] = 'application/json'
-  File.read('data.json')
+get '/' do
+  # NOTE: using haml as our template format
+  haml :index, format: :html5
 end
 
 get '/favorites' do
-  file = JSON.parse(File.read('data.json'))
-  unless params[:name] && params[:oid]
-    return 'Invalid Request'
-  movie = { name: params[:name], oid: params[:oid] }
-  file << movie
-  File.write('data.json',JSON.pretty_generate(file))
+  read_file
+end
+
+# NOTE: when we are trying to persist new data in
+# the backend, we should use POST request
+post '/favorites' do
+  return 'Invalid Request' unless params[:name] && params[:oid]
+
+  favorites = read_data
+  movie     = { name: params[:name], oid: params[:oid] }
+
+  favorites << movie
+  write_data(favorites)
+
   movie.to_json
 end
